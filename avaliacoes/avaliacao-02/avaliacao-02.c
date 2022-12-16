5c91e301ff87ed76ffbe6606cd8dd6433077090d
leansmonte <111462938+leansmonte@users.noreply.github.com>
2022-12-16 10:44:12 -0300

    Update avaliacao-02.c

--- avaliacoes/avaliacao-02/avaliacao-02.c
+++ avaliacoes/avaliacao-02/avaliacao-02.c
@@ -1,1 +1,30 @@
-em linguagem c
+
+b)
+
+#include <stdio.h>
+int main()
+{
+  int i;
+  for (i=0;i<=100;i=i+5)
+  { 
+    if (i%2==0)
+      printf("%d"\n", i);
+  }
+    return 0;
+ }
+ 
+ 2.a)
+ 
+ #include <stdio.h>
+ int main() 
+{
+  int i = 1;
+  while (i < 1000) 
+  { 
+    i++;
+    if (i%4!=0) 
+      continue;
+    printf("%d\n", i);  
+  } 
+   return 0;
+}
